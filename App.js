adf74b77fa0c8fc334b504b596fe6683873ec504
Yeison2017 <yha.big@gmail.com>
2020-10-03 23:14:39 -0500

    171 Manejadno el state con Context y useReducer

--- App.js
+++ App.js
@@ -11,70 +11,75 @@ import FormularioPlatillo from './views/FormularioPlatillo';
 import ResumenPedido from './views/ResumenPedido';
 import ProgresoPedido from './views/ProgresoPedido';
 
+// importar state de context
+import FirebaseState from './context/firebase/firebaseState';
+
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: '#FFDA00',
-            },
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            },
-          }}>
-          <Stack.Screen
-            name="NuevaOrden"
-            component={NuevaOrden}
-            options={{
-              title: 'Nueva Orden',
-            }}
-          />
+      <FirebaseState>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerStyle: {
+                backgroundColor: '#FFDA00',
+              },
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              },
+            }}>
+            <Stack.Screen
+              name="NuevaOrden"
+              component={NuevaOrden}
+              options={{
+                title: 'Nueva Orden',
+              }}
+            />
 
-          <Stack.Screen
-            name="Menu"
-            component={Menu}
-            options={{
-              title: 'Nuestro menú',
-            }}
-          />
+            <Stack.Screen
+              name="Menu"
+              component={Menu}
+              options={{
+                title: 'Nuestro menú',
+              }}
+            />
 
-          <Stack.Screen
-            name="DetallePlatillo"
-            component={DetallePlatillo}
-            options={{
-              title: 'Detella Platillo',
-            }}
-          />
+            <Stack.Screen
+              name="DetallePlatillo"
+              component={DetallePlatillo}
+              options={{
+                title: 'Detella Platillo',
+              }}
+            />
 
-          <Stack.Screen
-            name="FormularioPlatillo"
-            component={FormularioPlatillo}
-            options={{
-              title: 'Ordenar Platillo',
-            }}
-          />
+            <Stack.Screen
+              name="FormularioPlatillo"
+              component={FormularioPlatillo}
+              options={{
+                title: 'Ordenar Platillo',
+              }}
+            />
 
-          <Stack.Screen
-            name="ResumenPedido"
-            component={ResumenPedido}
-            options={{
-              title: 'Resumen Pedido',
-            }}
-          />
+            <Stack.Screen
+              name="ResumenPedido"
+              component={ResumenPedido}
+              options={{
+                title: 'Resumen Pedido',
+              }}
+            />
 
-          <Stack.Screen
-            name="ProgresoPedido"
-            component={ProgresoPedido}
-            options={{
-              title: 'Progreso de Pedido',
-            }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+            <Stack.Screen
+              name="ProgresoPedido"
+              component={ProgresoPedido}
+              options={{
+                title: 'Progreso de Pedido',
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </FirebaseState>
     </>
   );
 };
---
+++ context/firebase/firebaseState.js
@@ -0,0 +1,26 @@
+import React, {useReducer} from 'react';
+
+import FirebaseReducer from './firebaseReducer';
+import FirebaseContext from './firebaseContext';
+import firebaseReducer from './firebaseReducer';
+
+const FirebaseState = (props) => {
+  // Crear state inicial
+  const initialState = {
+    menu: [],
+  };
+
+  // useReducer con dispatch para ejecutar las funciones
+  const [state, dispatch] = useReducer(firebaseReducer, initialState);
+
+  return (
+    <FirebaseContext.Provider
+      value={{
+        menu: state.menu,
+      }}>
+      {props.children}
+    </FirebaseContext.Provider>
+  );
+};
+
+export default FirebaseState;
