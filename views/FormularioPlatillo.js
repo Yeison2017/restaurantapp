1756809070725b1a36143e87743ca83d908c8b40
Yeison2017 <yha.big@gmail.com>
2020-10-05 20:13:38 -0500

    184 El Grid de Nativebase

--- views/FormularioPlatillo.js
+++ views/FormularioPlatillo.js
@@ -1,8 +1,43 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {StyleSheet} from 'react-native';
+import {
+  Container,
+  Content,
+  Form,
+  Icon,
+  Input,
+  Grid,
+  Col,
+  Button,
+  Text,
+} from 'native-base';
+import {useNavigation} from '@react-navigation/native';
+import globalStyles from '../styles/global';
+
+import PedidoContext from '../context/pedidos/pedidosContext';
+import globlalStyles from '../styles/global';
 
 const FormularioPlatillo = () => {
-  return <Text>FormularioPlatillo</Text>;
+  return (
+    <Container>
+      <Content>
+        <Form>
+          <Text style={globalStyles.titulo}>Cantidad</Text>
+          <Grid>
+            <Col>
+              <Button>
+                <Icon name="remove" />
+              </Button>
+            </Col>
+            <Col></Col>
+            <Col>
+              <Icon name="add" />
+            </Col>
+          </Grid>
+        </Form>
+      </Content>
+    </Container>
+  );
 };
 
 export default FormularioPlatillo;
